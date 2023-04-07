bc093a2ecd345a317c91666913b5fded8a543ee4
blok98 <i.have.never.used.this.acc@gmail.com>
2023-04-07 15:53:52 +0200

    Zeef - working sequential sieve for primes.

--- src/eratosthenes.cpp
+++ src/eratosthenes.cpp
@@ -2,15 +2,44 @@
 #include <climits>
 #include <mpi.h>
 #include <vector>
+#include <cmath>
+
 using namespace std;
 
-void zeef(int N){
-    vector<bool> primes[N];
-    // loop door een lijst van ints a-b
-    // vind multiples tot b
+// loops over nonprimes and returns false if number is in the nonprimes
+bool is_prime(int number, vector<int> non_primes){
+    for (int i=0; i<non_primes.size(); i++){
+        if (number==non_primes[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> zeef(int N){
+    vector<int> non_primes;
+    vector<int> primes;
+    int k=2;
+    // first we loop over all elements until N
+    for (int i = k; i<N; i++){
+        // Than we loop until loop value is when k^2>n (j in our case)
+        for (int j=k; pow(j,2)<N; j++){
+            // all multiples of i{2..N} with factor of j{2..root(N)} are non primes
+            non_primes.push_back(i*j);
+        }
+        // all values declared to i {2..N} are primes, if not in non_primes.
+        if (is_prime(i, non_primes)){
+            primes.push_back(i);
+        }
+    }
+    return primes;
 }
 
 int main(){
-    zeef(10);
+    vector<int> a = zeef(10);
+    for (int i = 0; i < a.size(); i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
 
 }
